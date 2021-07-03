a1e889730df893917e20f7d6f85d7ee1202515e9
Aryan <aryan2563737@gmail.com>
2021-07-03 06:32:53 +0530

    Requests pointed to right API Endpoints

--- src/API_Endpoints.ts
+++ src/API_Endpoints.ts
@@ -12,7 +12,9 @@ enum methods {
 }
 
 const baseUrl =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:5000"
+  process.env.NODE_ENV === "production"
+    ? "https://awesomeeeee.herokuapp.com"
+    : "http://localhost:5000"
 
 export const updateContact: endpoint = {
   URL: `${baseUrl}/user/updateContact`,
